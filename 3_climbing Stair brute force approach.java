6fa653ef28758a70f52a8d310ae38d41a9047149
Amishra078 <mishraabhijeet078@gmail.com>
2026-06-10 22:10:18 +0530

    Climbing Stair brute force approach

---
+++ 3_climbing Stair brute force approach.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2){
+            return n;
+        }
+        
+        int x=1;
+        int y=2;
+        for(int i=3;i<n;i++){
+         int temp=x+y;
+         x=y;
+         y=temp;
+       }
+
+       return x+y;
+    }
+}
